4b6486ac73f80b33e7505d69a3fbeda6c8b803e4
Asmaa Yassin <150184697+AsmaaYassinDev@users.noreply.github.com>
2025-08-02 00:27:57 +0000

    initial commit with ProblemSolving project setup and first solution

---
+++ C#/ProblemSolving/Problems/BestTimeToBuyStock.cs
@@ -0,0 +1,19 @@
+namespace ProblemSolving.Problems
+{
+    public static class BestTimeToBuyStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int minPrice = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach (int price in prices)
+            {
+                minPrice = Math.Min(minPrice, price);
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+            }
+
+            return maxProfit;
+        }
+    }
+}
---
+++ C#/ProblemSolving/Program.cs
@@ -0,0 +1,6 @@
+﻿using ProblemSolving.Problems;
+
+int[] prices = { 7, 1, 5, 3, 6, 4 };
+Console.WriteLine("Max Profit: " + BestTimeToBuyStock.MaxProfit(prices));
+
+//121. Best Time to Buy and Sell Stock
